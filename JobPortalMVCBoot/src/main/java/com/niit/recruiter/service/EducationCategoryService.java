d649eb3e4b6f0ec18c213535a40e16f38cca166c
root <root@deep>
2020-04-07 03:47:32 +0530

    EducationCategory Service Created

---
+++ JobPortalMVCBoot/src/main/java/com/niit/recruiter/service/EducationCategoryService.java
@@ -0,0 +1,30 @@
+package com.niit.recruiter.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.niit.recruiter.model.EducationCategory;
+import com.niit.recruiter.repository.EducationCategoryRepository;
+
+@Service
+@Transactional
+public class EducationCategoryService {
+
+	@Autowired
+	private EducationCategoryRepository educationCategoryRepo;
+	
+	public List<EducationCategory> saveAll(List<EducationCategory> educationCategoryList)
+	{
+		return educationCategoryRepo.saveAll(educationCategoryList);
+	}
+
+	public EducationCategory findByEducationCategoryName(String educationCategoryName) {
+		// TODO Auto-generated method stub
+		return educationCategoryRepo.findByEducationCategoryName(educationCategoryName).get();
+	}
+	
+}
