b4ef02a420a87326271260a2dbca84cc24229118
deepanshu102 <deepanshujindal1021997@gmail.com>
2020-04-07 00:24:31 +0530

    Change in JobSeekerRepo String to Integer

--- JobPortalMVCBoot/src/main/java/com/niit/recruiter/repository/JobSeekerRepository.java
+++ JobPortalMVCBoot/src/main/java/com/niit/recruiter/repository/JobSeekerRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.niit.recruiter.model.JobSeeker;
 
-public interface JobSeekerRepository extends JpaRepository<JobSeeker, String> {
+public interface JobSeekerRepository extends JpaRepository<JobSeeker, Integer> {
 
 }
