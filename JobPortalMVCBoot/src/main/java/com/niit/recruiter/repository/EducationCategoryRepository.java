f52cc4a1754681062dbfa0ad71946ede1d9f541b
root <root@deep>
2020-04-09 22:52:20 +0530

    findAllByOrderByEducationCategoryIdAsc added

--- JobPortalMVCBoot/src/main/java/com/niit/recruiter/repository/EducationCategoryRepository.java
+++ JobPortalMVCBoot/src/main/java/com/niit/recruiter/repository/EducationCategoryRepository.java
@@ -1,5 +1,6 @@
 package com.niit.recruiter.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,7 @@ import com.niit.recruiter.model.EducationCategory;
 public interface EducationCategoryRepository extends JpaRepository<EducationCategory, Integer> {
 
 	Optional<EducationCategory> findByEducationCategoryName(String educationCategoryName);
+
+	List<EducationCategory> findAllByOrderByEducationCategoryIdAsc();
 
 }
