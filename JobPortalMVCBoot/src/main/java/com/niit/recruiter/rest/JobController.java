50b33c7841a8a61e21c256fbab232048ed3ec19c
Shubham Dubey <shubhamdubey1996@gmail.com>
2020-04-18 02:26:49 +0530

    .

---
+++ JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/JobController.java
@@ -0,0 +1,30 @@
+package com.niit.recruiter.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.niit.recruiter.model.Job;
+import com.niit.recruiter.model.Recruiter;
+import com.niit.recruiter.repository.JobRepository;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/jobs")
+public class JobController {
+
+	@Autowired
+	private JobRepository jobRepo;
+
+	@GetMapping
+	public List<Job> jobList(@RequestBody Recruiter recruiter) {
+		System.out.println(recruiter.getFirstName());
+		return jobRepo.findByRecruiter(recruiter);
+	}
+
+}
