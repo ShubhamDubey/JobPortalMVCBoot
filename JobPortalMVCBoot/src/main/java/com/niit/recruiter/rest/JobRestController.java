47120582f4904e5bfa83372b538f02cd131ddcaf
deepanshu102 <deepanshujindal1021997@gmail.com>
2020-04-19 01:40:06 +0530

    Posted Job Module Added

--- JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/JobRestController.java
+++ JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/JobRestController.java
@@ -9,13 +9,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.niit.recruiter.model.Application;
 import com.niit.recruiter.model.Job;
-import com.niit.recruiter.model.JobSeeker;
 import com.niit.recruiter.model.Recruiter;
 import com.niit.recruiter.repository.ApplicationRepository;
 import com.niit.recruiter.repository.JobRepository;
 import com.niit.recruiter.repository.JobSeekerRepository;
+import com.niit.recruiter.repository.RecruiterRepository;
 
 @CrossOrigin(origins = {"http://localhost:4200","http://localHost:8080"})
 @RestController
@@ -24,6 +23,8 @@ public class JobRestController {
 
 	@Autowired
 	private JobRepository jobRepo;
+	@Autowired
+	private RecruiterRepository recruiterRepo;
 	@Autowired
 	private JobSeekerRepository jobSeekerRepo;
 	@Autowired
@@ -34,6 +35,11 @@ public class JobRestController {
 		System.out.println(recruiter.getFirstName());
 		List<Job> jobList=jobRepo.findByRecruiter(recruiter);
 		return jobList;
+	}
+	@PostMapping("/postJob")
+	public List<Job> postJob(@RequestBody Recruiter recruiter)
+	{
+		return jobRepo.findByRecruiter(recruiterRepo.save(recruiter));
 	}
 
 }
