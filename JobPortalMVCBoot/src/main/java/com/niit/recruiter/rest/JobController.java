ca12a1d771af0f3ef1ae8cc4db5f4d510bd80c0d
deepanshu102 <deepanshujindal1021997@gmail.com>
2020-04-18 23:27:29 +0530

    JobListPostedByRecruiter

--- JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/JobController.java
+++ JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/JobController.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +21,7 @@ public class JobController {
 	@Autowired
 	private JobRepository jobRepo;
 
-	@GetMapping("")
+	@PostMapping("/showPostedJobs")
 	public List<Job> jobListPostedByRecruiter(@RequestBody Recruiter recruiter) {
 		System.out.println(recruiter.getFirstName());
 		List<Job> jobList=jobRepo.findByRecruiter(recruiter);
