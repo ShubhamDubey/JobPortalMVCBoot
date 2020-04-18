9a0a05c9645efa87c530f3e8d3071d25855fc590
deepanshu102 <deepanshujindal1021997@gmail.com>
2020-04-19 00:21:52 +0530

    Application Controller Added

--- JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/ApplicationController.java
+++ JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/ApplicationController.java
@@ -10,17 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.niit.recruiter.model.Application;
-import com.niit.recruiter.model.Job;
 import com.niit.recruiter.model.JobSeeker;
-import com.niit.recruiter.model.Recruiter;
 import com.niit.recruiter.repository.ApplicationRepository;
 import com.niit.recruiter.repository.JobRepository;
 import com.niit.recruiter.repository.JobSeekerRepository;
 
+
 @CrossOrigin(origins = {"http://localhost:4200","http://localHost:8080"})
 @RestController
-@RequestMapping("/api/jobs")
-public class JobController {
+@RequestMapping("/api/application")
+public class ApplicationController {
 
 	@Autowired
 	private JobRepository jobRepo;
@@ -28,13 +27,6 @@ public class JobController {
 	private JobSeekerRepository jobSeekerRepo;
 	@Autowired
 	private ApplicationRepository applicationRepo;
-	
-	@PostMapping("/showPostedJobs")
-	public List<Job> jobListPostedByRecruiter(@RequestBody Recruiter recruiter) {
-		System.out.println(recruiter.getFirstName());
-		List<Job> jobList=jobRepo.findByRecruiter(recruiter);
-		return jobList;
-	}
 	
 	@PostMapping("/applyJob")
 	public List<Application> applyJob(@RequestBody JobSeeker jobSeeker) {
