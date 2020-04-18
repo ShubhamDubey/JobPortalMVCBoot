102beea67831d36591301a345be3fe2f1dc015c5
niteshsahu7277 <nitesh.sahu7277@gmail.com>
2020-04-18 13:57:22 +0530

    Mockito,Swagger Dependency, HomeControllerTest

---
+++ JobPortalMVCBoot/src/test/java/com/niit/recruiter/HomeControllerTest.java
@@ -0,0 +1,15 @@
+package com.niit.recruiter;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.niit.recruiter.controller.HomeController;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {HomeController.class})
+public class HomeControllerTest {
+
+}
