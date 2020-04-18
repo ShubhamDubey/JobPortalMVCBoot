05ee64770dd0324ce10d6ae355312a0e0318d3aa
deepanshu102 <deepanshujindal1021997@gmail.com>
2020-04-18 14:25:03 +0530

    Swagger Documentation Added

--- JobPortalMVCBoot/src/main/java/com/niit/recruiter/JobPortalMvcBootApplication.java
+++ JobPortalMVCBoot/src/main/java/com/niit/recruiter/JobPortalMvcBootApplication.java
@@ -2,6 +2,9 @@ package com.niit.recruiter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JobPortalMvcBootApplication {
@@ -9,5 +12,8 @@ public class JobPortalMvcBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JobPortalMvcBootApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
