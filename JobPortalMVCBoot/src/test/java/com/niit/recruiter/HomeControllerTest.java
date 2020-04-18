7e4c2147e6abd4d093afed4b89eac0dfce05f0bc
niteshsahu7277 <nitesh.sahu7277@gmail.com>
2020-04-18 14:40:52 +0530

    HomeController Index Succesfully Running

--- JobPortalMVCBoot/src/test/java/com/niit/recruiter/HomeControllerTest.java
+++ JobPortalMVCBoot/src/test/java/com/niit/recruiter/HomeControllerTest.java
@@ -1,15 +1,49 @@
 package com.niit.recruiter;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 
 import com.niit.recruiter.controller.HomeController;
-
-
+import com.niit.recruiter.service.UsersService;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {HomeController.class})
+@SpringBootTest(classes = { HomeController.class })
 public class HomeControllerTest {
+
+	@InjectMocks
+	HomeController homeController;
+
+	@Mock
+	UsersService usersService;
+
+	@Spy
+	Model model;
+	@Spy
+	ModelMap modelMap;
+
+	@Spy
+	HttpServletRequest req;
+
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void viewRegisterPageTest() {
+		Assert.assertEquals(homeController.indexView(modelMap), "index");
+	}
 
 }
