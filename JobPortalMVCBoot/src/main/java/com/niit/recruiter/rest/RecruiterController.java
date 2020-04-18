268ce10d6b5bfa41341d0770ef85c3d5427f2a46
deepanshu102 <deepanshujindal1021997@gmail.com>
2020-04-18 23:13:06 +0530

    Runnign test

--- JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/RecruiterController.java
+++ JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/RecruiterController.java
@@ -3,7 +3,7 @@ package com.niit.recruiter.rest;
 public class RecruiterController {
 
 	public RecruiterController() {
-		// TODO Auto-generated constructor stub
+		System.out.println("Chal gaya");
 	}
 
 }
