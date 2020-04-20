4769962e5d87e83e3367a7b18861bb9f4dade233
niteshsahu7277 <nitesh.sahu7277@gmail.com>
2020-04-20 23:17:18 +0530

    Remove Mapping Rest Recruiter Controller

--- JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/RecruiterRestController.java
+++ JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/RecruiterRestController.java
@@ -4,13 +4,18 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.niit.recruiter.model.Recruiter;
+
 @CrossOrigin(origins = {"http://localhost:4200","http://localHost:8080"})
 @RestController
 @RequestMapping("/api/recruiter")
 public class RecruiterRestController {
 
-	public void RecruiterRestController() {
-		System.out.println("Chal gaya");
+	private Recruiter removeMapping( Recruiter recruiter)
+	{
+		recruiter.setUsers(null);
+		recruiter.setJobList(null);
+		return recruiter;
 	}
-
+	
 }
