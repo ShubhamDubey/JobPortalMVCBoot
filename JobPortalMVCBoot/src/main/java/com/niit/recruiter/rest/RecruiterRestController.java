3fa3cd916ec970791a7885d080eb142e84117ce9
niteshsahu7277 <nitesh.sahu7277@gmail.com>
2020-04-21 17:20:20 +0530

    Recruiter Rest Controller For The Angular Recruiter Profile

--- JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/RecruiterRestController.java
+++ JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/RecruiterRestController.java
@@ -1,21 +1,48 @@
 package com.niit.recruiter.rest;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.niit.recruiter.model.Recruiter;
+import com.niit.recruiter.model.Users;
+import com.niit.recruiter.repository.UsersRepository;
 
 @CrossOrigin(origins = {"http://localhost:4200","http://localHost:8080"})
 @RestController
 @RequestMapping("/api/recruiter")
 public class RecruiterRestController {
-
+	@Autowired
+	private UsersRepository usersRepo;
 	private Recruiter removeMapping( Recruiter recruiter)
 	{
 		recruiter.setUsers(null);
 		recruiter.setJobList(null);
 		return recruiter;
 	}
+	
+	
+	@PostMapping("/recruiterProfile")
+	public Map<String, String> recruietrProfile(@RequestBody Users users) {
+		Map<String,String> response=new HashMap<String, String>();
+		System.out.println(users.getId());
+		users=usersRepo.findById(users.getId()).get();
+		Recruiter recruiter=users.getRecruiter();
+		System.out.println("recruiter "+recruiter);
+		System.out.println("users "+users);
+		response.put("firstName",recruiter.getFirstName());
+		response.put("lastName",recruiter.getLastName());
+		response.put("email",users.getEmail());
+		return response;
+	}
+
+
+	
 	
 }
