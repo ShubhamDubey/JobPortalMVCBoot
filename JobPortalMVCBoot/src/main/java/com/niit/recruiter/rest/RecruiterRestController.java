8738b733cc80fe381a1b2307e3c439d7bad3aa69
Shubham Dubey <shubhamdubey1996@gmail.com>
2020-04-22 23:53:59 +0530

    Recruiters profile update completed

--- JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/RecruiterRestController.java
+++ JobPortalMVCBoot/src/main/java/com/niit/recruiter/rest/RecruiterRestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.niit.recruiter.model.Users;
 import com.niit.recruiter.repository.UsersRepository;
 
-@CrossOrigin(origins = {"http://localhost:4200","http://localHost:8080"})
+@CrossOrigin(origins = { "http://localhost:4200", "http://localHost:8080" })
 @RestController
 @RequestMapping("/api/recruiter")
 public class RecruiterRestController {
@@ -21,72 +21,50 @@ public class RecruiterRestController {
 	@Autowired
 	private UsersRepository usersRepo;
 
-
 	@PostMapping
-	public Map<String,String> profile(@RequestBody Map<String,String> users)
-	{
-		System.out.println("Recruiter Profile Called");
-		Map<String,String> profileResponse=new HashMap<String,String>();
-		if(users!=null)
-		{
-			Integer id=Integer.parseInt(users.get("id"));
-			
-			Users userObj=usersRepo.findById(id).get();
-			profileResponse.put("firstname",userObj.getRecruiter().getFirstName());
-			profileResponse.put("lastname",userObj.getRecruiter().getLastName());
-			profileResponse.put("password",userObj.getPassword());
-			profileResponse.put("email",userObj.getEmail());
-			profileResponse.put("id",userObj.getId()+"");
+	public Map<String, String> profile(@RequestBody Map<String, String> users) {
+		Map<String, String> profileResponse = new HashMap<String, String>();
+		if (users != null) {
+			Integer id = Integer.parseInt(users.get("id"));
+
+			Users userObj = usersRepo.findById(id).get();
+			profileResponse.put("firstname", userObj.getRecruiter().getFirstName());
+			profileResponse.put("lastname", userObj.getRecruiter().getLastName());
+			profileResponse.put("password", userObj.getPassword());
+			profileResponse.put("email", userObj.getEmail());
+			profileResponse.put("id", userObj.getId() + "");
 		}
 		return profileResponse;
-	}	
-	
-	@PostMapping(value="/updateprofile")
-	public Map<String,String> updateProfile(@RequestBody Map<String,String> users)
-	{
-		System.out.println("Recruiter Update Profile Called");
-		Map<String,String> profileResponse=new HashMap<String,String>();
-		System.out.println("DATA"+users.get("firstname"));
-		if(users!=null)
-		{		
-			Integer id=Integer.parseInt(users.get("id"));
-			String firstName=users.get("firstname");
-			String lastName=users.get("lastname");
-			String email=users.get("email");
-			Users user=usersRepo.findById(id).get();
-			
-			user.setEmail(email);
-			user.getRecruiter().setFirstName(firstName);
-			user.getRecruiter().setLastName(lastName);
-			usersRepo.save(user);
-		}
-		else
-		{
-			users=null;
-		}
+	}
+
+	@PostMapping(value = "/updateprofile")
+	public Map<String, String> updateProfile(@RequestBody Map<String, String> users) {
+
+		Integer id = Integer.parseInt(users.get("id"));
+		String firstName = users.get("firstname");
+		String lastName = users.get("lastname");
+		String email = users.get("email");
+		Users user = usersRepo.findById(id).get();
+
+		user.setEmail(email);
+		user.getRecruiter().setFirstName(firstName);
+		user.getRecruiter().setLastName(lastName);
+		usersRepo.save(user);
+
 		return users;
-	}	
+	}
+
 	@PostMapping("/changepassword")
-	public Map<String,String> changePassword(@RequestBody Map<String,String> users)
-	{
-		System.out.println("Recruiter Update Profile Called");
-		Map<String,String> profileResponse=new HashMap<String,String>();
-		System.out.println("DATA"+users.get("firstname"));
-		if(users!=null)
-		{		
-			Integer id=Integer.parseInt(users.get("id"));
-		
-			String password=users.get("updatedpassword");
-			Users user=usersRepo.findById(id).get();
-			user.setPassword(password);
-			usersRepo.save(user);
-			users=users;
-			
-		}
-		else
-		{
-			users=null;
-		}
+	public Map<String, String> changePassword(@RequestBody Map<String, String> users) {
+
+		Integer id = Integer.parseInt(users.get("id"));
+
+		String password = users.get("password");
+
+		Users user = usersRepo.findById(id).get();
+		user.setPassword(password);
+		usersRepo.save(user);
+
 		return users;
-	}	
+	}
 }
